5094c7feb59f7c87ba0eb38cd60374591c8069f3
MoRsHeD <mdmorshed0187@gmail.com>
2025-11-25 12:09:07 +0600

    created proxy file for protecting route

---
+++ proxy.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import jwt from "jsonwebtoken";
+import { deleteCookie, getCookie } from './services/auth/tokenHandlers';
+
+export async function proxy(request: NextRequest) {
+    const accessToken = await getCookie("accessToken");
+    const pathname = request.nextUrl.pathname;
+
+    const authPages = ['/login', '/register'];
+    const protectedRoutes = [
+        '/feed',
+    ];
+
+    if (accessToken && authPages.some(path => pathname.startsWith(path))) {
+        return NextResponse.redirect(new URL('/feed', request.url));
+    }
+
+    if (!accessToken && protectedRoutes.some(path => pathname.startsWith(path))) {
+        const loginUrl = new URL('/login', request.url);
+        return NextResponse.redirect(loginUrl);
+    }
+
+    if (accessToken) {
+        try {
+            jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET as string);
+        } catch (err) {
+            await deleteCookie("accessToken");
+            await deleteCookie("refreshToken");
+
+            const loginUrl = new URL('/login', request.url);
+            return NextResponse.redirect(loginUrl);
+        }
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.well-known).*)',
+    ],
+};
